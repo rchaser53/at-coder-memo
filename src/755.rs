947c8e6dcc06005a0dfee9af3ecbc7c49c120d5a
rchaser53 <tayoshizawa29@gmail.com>
2020-09-08 22:00:59 +0900

    update improve methods

--- src/755.rs
+++ src/755.rs
@@ -40,3 +40,63 @@ fn main() {
   }
   println!("{}", count);
 }
+
+
+use proconio::input;
+
+struct Helper {
+  pub count: usize,
+  pub n: usize
+}
+
+impl Helper {
+  fn traverse(&mut self, i: usize, v: usize) {
+    if i == 10 {
+      if v <= self.n && self.is_safe(v) {
+        self.count += 1;
+      }
+      return
+    }
+      
+    self.traverse(i+1, v+ 10usize.pow(i as u32)*7);
+    self.traverse(i+1, v+ 10usize.pow(i as u32)*5);
+    self.traverse(i+1, v+ 10usize.pow(i as u32)*3);
+
+    if v <= self.n && self.is_safe(v) {
+      self.count += 1;
+    }
+  }
+  
+  fn is_safe(&self, mut val: usize) -> bool {
+    let mut f3 = false;
+    let mut f5 = false;
+    let mut f7 = false;
+    
+    while 0 < val {
+      let v = val % 10;
+      if v == 3 {
+        f3 = true;
+      } else if v == 5 {
+        f5 = true;
+      } else if v == 7 {
+        f7 = true;
+      } else {
+        f3 = false;
+        break
+      }
+      val /= 10;
+    }
+    
+    f3 && f5 && f7
+  }
+}
+
+fn main() {
+  input! {
+    n: usize,
+  }
+
+  let mut helper = Helper { count: 0, n };
+  helper.traverse(0, 0);
+  println!("{}", helper.count);
+}
