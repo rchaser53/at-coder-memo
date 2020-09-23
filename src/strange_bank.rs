f082fda48a00719a5fa64ddc683a089d60cdcb37
rchaser53 <tayoshizawa29@gmail.com>
2020-09-23 09:59:16 +0900

    update strange_bank.rs

--- src/strange_bank.rs
+++ src/strange_bank.rs
@@ -50,3 +50,40 @@ fn main() {
   let mut helper = Helper::new(n);  
   println!("{}", helper.traverse(n));
 }
+
+use proconio::input;
+
+fn main() {
+  input! {
+    n: usize,
+  }
+  
+  let max_value = 100_000;
+  let mut dp: Vec<usize> = vec![max_value+1;max_value+1];
+  let mut choices: Vec<usize> = vec![1];
+  dp[1] = 1;
+  
+  let mut v = 6;
+  while v <= max_value {
+    dp[v] = 1;
+    choices.push(v);
+    v *= 6;
+  }
+
+  let mut v = 9;
+  while v <= max_value {
+    dp[v] = 1;
+    choices.push(v);
+    v *= 9;
+  }
+  
+  for i in 2..=max_value {
+    for v in choices.iter() {
+      if &i < v { continue }
+      let mut temp = std::cmp::min(dp[i-v]+1, dp[i-1]+1);
+      dp[i] = std::cmp::min(dp[i], temp);
+    }
+  }
+  
+  println!("{}", dp[n]);
+}
