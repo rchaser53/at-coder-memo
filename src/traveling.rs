e79703f79f729a5171d0f643ce0deed08d8392ee
rchaser53 <tayoshizawa29@gmail.com>
2020-09-30 21:19:45 +0900

    update traveling.rs

--- src/traveling.rs
+++ src/traveling.rs
@@ -1,30 +1,27 @@
-#[warn(non_snake_case)]
+#![allow(unused_imports)]
+use proconio::input;
+use proconio::marker::*;
+use std::collections::VecDeque;
+ 
 fn main() {
-    let mut s = String::new();
-    std::io::stdin().read_line(&mut s).unwrap();
-    let n = s.trim_end().parse().unwrap();
-
-  	let mut last: Vec<i32> = vec![0, 0, 0];
-  
-    for _ in 0..n {
-        let mut s = String::new();
-        std::io::stdin().read_line(&mut s).unwrap();
-        let a:Vec<i32> = s
-            .split_whitespace()
-            .map(|x| x.parse::<i32>().unwrap())
-            .collect::<Vec<i32>>();      
-      
-     	let t = a[0] - last[0];
-     	let x = (a[1] - last[1]).abs();
-     	let y = (a[2] - last[2]).abs();
-        let sum = x + y;
-      	if t - sum < 0 || sum % 2 != t % 2 {
-      		println!("No");
-            return
-        }
-        
-        last = a;
+  input!{
+    n: usize,
+    vals: [(isize,isize,isize);n],
+  }
+  let mut last = (0, 0, 0);
+  for i in 0..n {
+    let val = vals[i];
+    let diff_t = val.0 - last.0;
+    let need_val = (val.1 - last.1).abs() + (val.2 - last.2).abs();
+    if diff_t < need_val {
+      println!("No");
+      return
+    } else if (diff_t - need_val) % 2 == 1 {
+      println!("No");
+      return
     }
-    
-	println!("Yes");
+    last = val;
+  }
+  
+  println!("Yes");
 }
