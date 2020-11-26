d962bd8f12be9d4092994250deb55706f579deeb
rchaser53 <tayoshizawa29@gmail.com>
2020-11-27 00:03:09 +0900

    add abc32a.rs

---
+++ src/abc32a.rs
@@ -0,0 +1,32 @@
+#![allow(unused_imports)]
+use proconio::{input, fastout};
+use proconio::marker::*;
+use std::collections::*;
+use std::cmp::Ordering;
+use petgraph::unionfind::UnionFind;
+
+fn gcv(a: usize, b: usize) -> usize { 
+  if b == 0 {
+    a
+  } else {
+    gcv(b, a % b)
+  }
+}
+
+fn lcm(a:usize, b:usize) -> usize {
+  a * b / gcv(a, b)
+}
+
+fn main() {
+  input!{
+    a: usize,
+    b: usize,
+    n: usize
+  }
+  let mut v = lcm(a, b);
+  let mut i = 1;
+  while v * i <  n {
+    i += 1;
+  }
+  println!("{}", v * i);
+}
