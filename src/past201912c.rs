326dcfa8ed2fb691503f84d4870811398fdf30f2
rchaser53 <tayoshizawa29@gmail.com>
2020-12-25 21:25:13 +0900

    add past201912c.rs

---
+++ src/past201912c.rs
@@ -0,0 +1,20 @@
+#![allow(unused_imports)]
+use proconio::{input, fastout};
+use proconio::marker::*;
+use std::collections::*;
+use std::cmp::*;
+use maplit::{btreemap, btreeset, hashmap, hashset};
+use petgraph::unionfind::UnionFind;
+use petgraph::algo::dijkstra;
+use petgraph::graph::{NodeIndex, DiGraph, UnGraph};
+use permutohedron::{Heap, heap_recursive};
+
+const MOD:usize = 1_000_000_007;
+
+fn main() {
+  input!{
+    mut vals: [usize;6]
+  }
+  vals.sort();
+  println!("{}", vals[3]);
+}
