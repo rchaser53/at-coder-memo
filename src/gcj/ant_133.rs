a32f62fb2ab3755a39c9bea45ba1614547179165
rchaser53 <tayoshizawa29@gmail.com>
2021-01-04 18:44:26 +0900

    add ant_133.rs

---
+++ src/gcj/ant_133.rs
@@ -0,0 +1,42 @@
+// 平均最大化
+
+fn check(
+  vals: &Vec<f64>,
+  x: f64,
+  k: usize
+) -> bool {
+  let mut copied = vec![0f64;vals.len()];
+  for i in 0..vals.len() {
+    let (w, v) = vals[i];
+    copied[i] = v - x * w;
+  }
+  copied.sort();
+  copied.reverse();
+  let mut sum = 0f64;
+  for i in 0..k {
+    sum += copied[i];
+  }
+
+  0 <= sum
+}
+
+// 要確認
+fn main() {
+  input!{
+    n: usize,
+    k: usize,
+    vals: [(f64,f64);n]
+  }
+
+  let mut min = 0f64;
+  let mut max = 1_000_000_000f64;
+  for i in 0..100 {
+    let mid = (min + max) / 2f64;
+    if check(&vals, mid, k) {
+      min = mid;
+    } else {
+      max = mid;
+    }
+  }
+  println!("{}", max);
+}
