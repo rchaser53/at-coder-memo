7b474acdc26ee69b551b0814bec54cece2c18812
rchaser53 <tayoshizawa29@gmail.com>
2021-01-16 20:42:54 +0900

    update div_rem_number.rs

--- src/div_rem_number.rs
+++ src/div_rem_number.rs
@@ -8,41 +8,19 @@ use petgraph::unionfind::UnionFind;
 #[fastout]
 fn main() {
   input!{
-    n: usize,
+    n: u128,
   }
  
   let mut result = 0;
-  let mut i = n;
-  let mut stack = vec![];
-  let mut v = 2;
-  while v * v <= i {
-    if i % v == 0 {
-      stack.push(v);
-      i /= v;
-    } else {
-      v += 1;
+  let mut i = 1;
+  loop {
+    let left = n - i;
+    let v = left / i;
+    if v <= i { break }
+    if left % i == 0 {
+      result += v;
     }
-  }
-  if 1 < i {
-    stack.push(i);
-  }
-  
-  let limit = 2usize.pow(stack.len() as u32);
-  let mut seen = HashSet::new();
-  for i in 0..limit {
-    let mut temp = 1;
-    for ii in 0..stack.len() {
-      if i >> ii & 1 == 1 {
-        temp *= stack[ii];
-      }
-    }
-    if !seen.contains(&temp) && 1 < temp {
-      let v = temp - 1;
-      if n % v == n / v {
-        result += v;
-      }
-      seen.insert(temp);
-    }
+    i += 1;
   }
   
   println!("{}", result);
