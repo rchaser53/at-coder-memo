7a623983f7d36f7c9170c2e755a22e1b607858c2
rchaser53 <tayoshizawa29@gmail.com>
2021-01-17 11:51:41 +0900

    update lcs.rs

--- src/lcs.rs
+++ src/lcs.rs
@@ -47,3 +47,80 @@ fn main() {
  
   println!("{}", result.iter().collect::<String>());
 }
+
+
+fn my_answer() {
+  input!{
+    s: Chars,
+    t: Chars,
+  }
+  
+  let mut dp = vec![vec![0;s.len()];t.len()];
+  if s[0] == t[0] {
+    dp[0][0] = 1;
+  }
+  
+  for i in 1..s.len() {
+    if s[i] == t[0] {
+      dp[0][i] = 1;
+    } else {
+      dp[0][i] = dp[0][i-1];
+    }
+  }
+  for i in 1..t.len() {
+    if t[i] == s[0] {
+      dp[i][0] = 1;
+    } else {
+      dp[i][0] = dp[i-1][0];
+    }
+  }
+  
+  for i in 1..t.len() {
+    for ii in 1..s.len() {
+      dp[i][ii] = std::cmp::max(dp[i-1][ii], dp[i][ii-1]);
+      if t[i] == s[ii] {
+        dp[i][ii] = std::cmp::max(dp[i][ii], dp[i-1][ii-1]+1);
+      }
+    }
+  }
+  
+  let mut result = vec![];
+  let mut r = t.len()-1;
+  let mut c = s.len()-1;
+ 
+  while 0 < r && 0 < c {
+    if dp[r][c] == dp[r-1][c] {
+      r -= 1;
+    } else if dp[r][c] == dp[r][c-1] {
+      c -= 1;
+    } else {
+      result.push(s[c].to_string());
+      r -= 1;
+      c -= 1;
+    }
+  }
+ 
+  if r == 0 && c == 0 {}
+  else if r == 0 {
+    while 0 < c {
+      if dp[r][c-1] < dp[r][c] {
+        result.push(s[c].to_string());
+      }
+      c -= 1;    
+    }
+  } else if c == 0 {
+    while 0 < r {
+      if dp[r-1][c] < dp[r][c] {
+        result.push(t[r].to_string());
+      }
+      r -= 1;    
+    }
+  }
+    
+  if dp[0][0] == 1 {
+    result.push(s[0].to_string());
+  }
+  
+  result.reverse();
+  println!("{}", result.into_iter().collect::<String>());
+}
