af07fdec873f4a652ece6d43e8aedd45fceadeb2
rchaser53 <tayoshizawa29@gmail.com>
2021-01-23 16:32:57 +0900

    update modulo_summation.rs

--- src/modulo_summation.rs
+++ src/modulo_summation.rs
@@ -1,14 +1,22 @@
-use proconio::input;
+#![allow(unused_imports)]
+use proconio::{input, fastout};
+use proconio::marker::*;
+use itertools::Itertools;
+use maplit::{btreemap, btreeset, hashmap, hashset};
+use petgraph::unionfind::UnionFind;
+use petgraph::algo::dijkstra;
+use petgraph::graph::{NodeIndex, DiGraph, UnGraph};
+use permutohedron::{Heap, heap_recursive};
+use std::collections::*;
+use std::cmp::*;
 
+const MOD:usize = 1_000_000_007;
+
+#[fastout]
 fn main() {
-  input! {
+  input!{
     n: usize,
-    vals: [usize; n]
+    vals: [Usize1;n]
   }
-  
-  let mut result = 0;
-  for v in vals {
-    result += v - 1;
-  }
-  println!("{}", result);
+  println!("{}", vals.into_iter().sum::<usize>());
 }
