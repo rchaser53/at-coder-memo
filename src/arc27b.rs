8725275f6357a41dfba7cdec5c96670a5a41d98e
rchaser53 <tayoshizawa29@gmail.com>
2021-02-16 22:57:27 +0900

    add answer to use unionfind

--- src/arc27b.rs
+++ src/arc27b.rs
@@ -91,3 +91,60 @@ fn main() {
     println!("{}", 9 * 10usize.pow((len-1) as u32));
   }
 }
+
+fn convert(c: char) -> usize {
+  if 'A' <= c && c <= 'Z' {
+    10 + c as usize - 'A' as usize
+  } else {
+    c as usize - '0' as usize
+  }
+}
+
+#[fastout]
+fn main() {
+  input!{
+    n: usize,
+    s1: Chars,
+    s2: Chars
+  }
+  
+  let mut unknown = HashSet::new();
+  let mut components = UnionFind::new(36);
+  for i in 0..n {
+    let v1 = convert(s1[i]);
+    let v2 = convert(s2[i]);
+    components.union(v1, v2);
+    if 10 <= v1 {
+      unknown.insert(s1[i]);
+    }
+    if 10 <= v2 {
+      unknown.insert(s2[i]);
+    }
+  }
+  
+  let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let digit = "0123456789";
+  for d in digit.chars() {
+    for c in alphabet.chars() {
+      let dv = convert(d);
+      let cv = convert(c);
+      if components.find(dv) == components.find(cv) {
+        unknown.remove(&c);
+      }
+    }
+  }
+  
+  let mut count = 0;
+  for c in alphabet.chars() {
+    let v = convert(c);
+    if unknown.contains(&c) && components.find(v) == v {
+      count += 1;
+    }
+  }
+  
+  if unknown.contains(&s1[0]) {
+    println!("{}", 9 * 10usize.pow((count-1) as u32));
+  } else {
+    println!("{}", 10usize.pow(count as u32));
+  }
+}
