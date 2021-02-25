157969a9896ac29692f233aff275551baacf6aaa
rchaser53 <tayoshizawa29@gmail.com>
2021-02-26 00:14:22 +0900

    rewrite by myself

--- src/arc38b.rs
+++ src/arc38b.rs
@@ -14,38 +14,40 @@ const MOD:usize = 1_000_000_007;
 
 fn is_win(
   rows: &Vec<Vec<char>>,
+  memo: &mut Vec<Vec<Option<bool>>>,
   r: usize,
-  c: usize,
-  memo: &mut Vec<Vec<Option<bool>>>
+  c: usize
 ) -> bool {
   if let Some(v) = memo[r][c] {
     v
   } else if rows[r][c] == '#' {
     true
   } else {
-    let v1 = !is_win(&rows, r+1, c, memo);
-    let v2 = !is_win(&rows, r+1, c+1, memo);
-    let v3 = !is_win(&rows, r, c+1, memo);
+    let v1 = !is_win(rows, memo, r+1, c);
+    let v2 = !is_win(rows, memo, r+1, c+1);
+    let v3 = !is_win(rows, memo, r, c+1);
     let v = v1 || v2 || v3;
     memo[r][c] = Some(v);
     v
   }
 }
- 
+
 fn main() {
   input!{
     h: usize,
     w: usize,
     mut rows: [Chars;h]
   }
+  
   for i in 0..h {
     rows[i].push('#');
   }
-  rows.push((0..w+1).map(|_| '#').collect::<Vec<char>>());
-  let mut memo = vec![vec![None;w+2];h+2];
-  if is_win(&rows, 0, 0, &mut memo) {
-    println!("First");  
+  rows.push(vec!['#';w+1]);
+  
+  let mut memo = vec![vec![None;w+1];h+1];
+  if is_win(&rows, &mut memo, 0, 0) {
+    println!("First");
   } else {
     println!("Second");
-  }
+  } 
 }
