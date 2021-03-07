28ff2f9b6a937593201367f9188f56dbf7752e53
rchaser53 <tayoshizawa29@gmail.com>
2021-03-07 17:26:46 +0900

    add abc194d.rs

--- src/abc194d.rs
+++ src/abc194d.rs
@@ -8,15 +8,22 @@ use petgraph::algo::dijkstra;
 use petgraph::graph::{NodeIndex, DiGraph, UnGraph};
 use permutohedron::{Heap, heap_recursive};
 use std::collections::*;
+use std::cmp::*;
 use superslice::Ext;
 
 const MOD:usize = 1_000_000_007;
-const MAX: usize = 1000;
 
+#[fastout]
 fn main() {
   input!{
-    h: usize,
-    w: usize
+    un: usize
   }
-  println!("{}", (w -1) * h + (h-1) * w);
+  let n = un as f64;
+  let mut result = 0f64;
+  for i in 1..un {
+    let v = i as f64;
+    result += n / (n - v);
+  }
+  println!("{}", result);
 }
+  
