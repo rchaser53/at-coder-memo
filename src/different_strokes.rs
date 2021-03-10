fb5c761b1fbb46cfd5b21ae28775a89a350f4475
rchaser53 <tayoshizawa29@gmail.com>
2021-03-10 23:03:50 +0900

    solve by myself

--- src/different_strokes.rs
+++ src/different_strokes.rs
@@ -32,3 +32,59 @@ fn main() {
   
   println!("{}", a - b);
 }
+
+fn culc(
+  n: usize,
+  a: &Vec<(usize, isize)>,
+  b: &Vec<(usize, isize)>,
+) -> isize {
+  let mut set = HashSet::new();
+  let mut av = vec![];
+  let mut bv = vec![];
+  let mut ai = 0;
+  let mut bi = 0;
+  let mut flag = true;
+  while ai < n || bi < n {
+    if flag {
+      flag = !flag;
+      while ai < n && set.contains(&a[ai].0) {
+        ai += 1;
+      }
+      if n <= ai { continue }
+      av.push(a[ai].1);
+      set.insert(a[ai].0);
+    } else {
+      flag = !flag;
+      while bi < n && set.contains(&b[bi].0) {
+        bi += 1;
+      }
+      if n <= bi { continue }
+      bv.push(b[bi].1);
+      set.insert(b[bi].0);
+    }
+  }
+  av.into_iter().sum::<isize>() - bv.into_iter().sum::<isize>()
+}
+
+#[fastout]
+fn main() {
+  input!{
+    n: usize,
+    vals: [(isize, isize);n]
+  }
+  
+  let mut base = vec![(0,0);n];
+  let mut a = base.clone();
+  let mut b = a.clone();
+  for i in 0..n {
+    base[i] = (i, vals[i].0 + vals[i].1);
+  }
+  base.sort_by(|a,b| a.1.cmp(&b.1));
+  base.reverse();
+  for i in 0..n {
+    a[i] = (base[i].0, vals[base[i].0].0);
+    b[i] = (base[i].0, vals[base[i].0].1);
+  }
+  
+  println!("{}", culc(n, &a, &b));
+}
