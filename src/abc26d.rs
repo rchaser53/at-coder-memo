ad25e4376cbf1ccb98fc750347e2ab9296978c21
rchaser53 <tayoshizawa29@gmail.com>
2021-03-20 11:10:01 +0900

    update abc26d.rs

--- src/abc26d.rs
+++ src/abc26d.rs
@@ -1,31 +1,34 @@
 #![allow(unused_imports)]
 use proconio::{input, fastout};
 use proconio::marker::*;
-use std::collections::*;
-use std::cmp::Ordering;
+use itertools::Itertools;
+use maplit::{btreemap, btreeset, hashmap, hashset};
 use petgraph::unionfind::UnionFind;
+use petgraph::algo::dijkstra;
+use petgraph::graph::{NodeIndex, DiGraph, UnGraph};
+use permutohedron::{Heap, heap_recursive};
+use superslice::*;
+use std::collections::*;
+use std::cmp::*;
 
 const MOD:usize = 1_000_000_007;
 
 fn main() {
   input!{
-    a: f64,
-    b: f64,
-    c: f64
+    a:f64,
+    b:f64,
+    c:f64
   }
   
   let mut left = 0f64;
-  let mut right = 1000f64;
-  let mut mid = 0f64;
-  for _ in 0..200 {
-    mid = (left + right) / 2f64;
-    let v = a * mid + b * (c * mid * std::f64::consts::PI).sin();
-    if v < 100f64 {
+  let mut right = 200f64;
+  while left + 0.00000000001 < right {
+    let mid = (left + right) / 2f64;
+    if a * mid + b * (c * mid * std::f64::consts::PI).sin() - 100f64 < 0f64 {
       left = mid;
     } else {
-      right = mid;
+      right = mid;    
     }
   }
-
-  println!("{}", right);
+  println!("{}", left);
 }
