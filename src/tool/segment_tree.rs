3fc9b7fe461a963379b0d3a28399e03373f36312
rchaser53 <tayoshizawa29@gmail.com>
2021-05-02 21:18:17 +0900

    add segment_tree.rs

---
+++ src/tool/segment_tree.rs
@@ -0,0 +1,49 @@
+// NEED TO EDIT
+type TreeType = usize;
+pub struct SegmentTree {
+  n: usize,
+  arr: Vec<TreeType>,
+  e: TreeType
+}
+
+impl SegmentTree {
+  pub fn new(n: usize, e:TreeType) -> Self {
+    let mut x = 1;
+    while x < n {
+      x *= 2;
+    }
+    SegmentTree {
+      n:x,
+      arr:vec![e;x*2],
+      e
+    }    
+  }
+   
+  pub fn update(&mut self, mut i: usize, val: TreeType) {
+    i += self.n - 1;
+    self.arr[i] = val;
+    while 0 < i {
+      i = (i-1) / 2;
+      self.arr[i] = std::cmp::max(self.arr[i*2+1], self.arr[i*2+2]);
+    }
+  }
+  
+  fn query(&self, a:usize, b:usize) -> TreeType {
+    self.query_sub(a, b, 0, 0, self.n)
+  }
+
+  fn query_sub(
+    &self, a:usize, b:usize,
+    k:usize, l:usize, r:usize
+  ) -> TreeType { 
+    if r <= a || b <= l {
+      0
+    } else if a <= l && r <= b {
+      self.arr[k]
+    } else {
+      let vl = self.query_sub(a, b, k*2+1, l, (l+r)/2);
+      let vr = self.query_sub(a, b, k*2+2, (l+r)/2, r);
+      std::cmp::max(vl, vr)
+    }
+  }
+}
