5a4b20751c033b00d6708dbf7c136cccc33925aa
rchaser53 <tayoshizawa29@gmail.com>
2021-05-04 22:51:12 +0900

    rename and add solution using segment tree

--- src/abc185f.rs
+++ src/abc185f.rs
@@ -78,3 +78,74 @@ pub mod fenwicktree {
     }
   }
 }
+
+fn main() {
+  input!{
+    n: usize,
+    q: usize,
+    vals: [usize;n],
+    queries: [(usize, usize, usize);q]
+  }
+  
+  let mut stree = SegmentTree::new(n, 0);
+  for i in 0..n {
+    stree.update(i, vals[i]);
+  }
+  
+  for (t, l, r) in queries {
+    if t == 1 {
+      stree.update(l-1, r);
+    } else {
+      println!("{}", stree.query(l-1, r));
+    }
+  }
+}
+ 
+type TreeType = usize;
+pub struct SegmentTree {
+  n: usize,
+  arr: Vec<TreeType>,
+  e: TreeType
+}
+impl SegmentTree {
+  pub fn new(n: usize, e:TreeType) -> Self {
+    let mut x = 1;
+    while x < n {
+      x *= 2;
+    }
+    SegmentTree {
+      n:x,
+      arr:vec![e;x*2],
+      e
+    }    
+  }
+   
+  pub fn update(&mut self, mut i: usize, val: TreeType) {
+    i += self.n-1;
+    self.arr[i] ^= val;
+    while 0 < i {
+      i = (i-1) / 2;
+      self.arr[i] = self.arr[i*2+1] ^ self.arr[i*2+2];
+    }
+  }
+  
+  // a以上、b未満になるのに注意
+  fn query(&self, a:usize, b:usize) -> TreeType {
+    self.query_sub(a, b, 0, 0, self.n)
+  }
+ 
+  fn query_sub(
+    &self, a:usize, b:usize,
+    k:usize, l:usize, r:usize
+  ) -> TreeType { 
+    if r <= a || b <= l {
+      0
+    } else if a <= l && r <= b {
+      self.arr[k]
+    } else {
+      let vl = self.query_sub(a, b, k*2+1, l, (l+r)/2);
+      let vr = self.query_sub(a, b, k*2+2, (l+r)/2, r);
+      vl ^ vr
+    }
+  }
+}
