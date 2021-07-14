5f60e5ba05da07a8436fad9fc7af10ded1c0bafd
rchaser53 <tayoshizawa29@gmail.com>
2021-07-14 23:02:50 +0900

    rewrite by myself

--- src/abc/abc201d.rs
+++ src/abc/abc201d.rs
@@ -1,73 +1,67 @@
 /* OUTPUT FILE */
-#![allow(unused_imports)]
 use proconio::input;
 use proconio::marker::*;
 use std::collections::*;
 
+fn helper(
+  rows: &Vec<Vec<char>>,
+  r: usize,
+  c: usize
+) -> isize {
+  if r == 0 && c == 0 {
+    return 0
+  }
+
+  if rows[r][c] == '+' {
+    if (r+c) % 2 == 1 {
+      1
+    } else {
+      -1
+    }
+  } else {
+    if (r+c) % 2 == 1 {
+      -1
+    } else {
+      1
+    }
+  }
+}
+
 pub fn main(
 ) {
-    input! {
-      h:usize,
-      w:usize,
-      rows:[Chars;h]
-    }
+  input! {
+    h:usize,
+    w:usize,
+    rows:[Chars;h]
+  }
 
-    let mut memo = vec![vec![0;w];h];
-    for i in (0..h-1).rev() {
-      memo[i][w-1] = memo[i+1][w-1];
-      let mut v = 1;
-      if (i+w-1) % 2 == 1 {
-        v = -1;
-      }
-      if rows[i+1][w-1] == '+' {
-        v *= 1;
-      } else {
-        v *= -1;
-      }
-      memo[i][w-1] += v;
-    }
-    for i in (0..w-1).rev() {
-      memo[h-1][i] = memo[h-1][i+1];
-      let mut v = 1;
-      if (i+h-1) % 2 == 1 {
-        v = -1;
-      }
-      if rows[h-1][i+1] == '+' {
-        v *= 1;
-      } else {
-        v *= -1;
-      }
-      memo[h-1][i] += v;
-    }
+  let mut memo = vec![vec![0;w];h];
+  memo[h-1][w-1] = helper(&rows, h-1, w-1);
 
-    for i in (0..h-1).rev() {
-      for j in (0..w-1).rev() {
-        let mut p = 1;
-        if (i+j) % 2 == 1 {
-          p = -1;
-        }
-        let vi = memo[i+1][j] + p * if rows[i+1][j] == '+' {
-          1
-        } else {
-          -1
-        };
-        let vj = memo[i][j+1] + p * if rows[i][j+1] == '+' {
-          1
-        } else {
-          -1
-        };
-        memo[i][j] = if (i+j) % 2 == 0 {
-          vi.max(vj)
-        } else {
-          vi.min(vj)
-        };
+  for i in (0..w-1).rev() {
+    memo[h-1][i] = memo[h-1][i+1] + helper(&rows, h-1, i);
+  }
+
+  for i in (0..h-1).rev() {
+    memo[i][w-1] = memo[i+1][w-1] + helper(&rows, i, w-1);
+  }
+
+  for r in (0..h-1).rev() {
+    for c in (0..w-1).rev() {
+      let v = helper(&rows, r, c);
+      if (r+c) % 2 == 0 {
+        memo[r][c] = std::cmp::max(memo[r+1][c], memo[r][c+1]) + v;
+      } else {
+        memo[r][c] = std::cmp::min(memo[r+1][c], memo[r][c+1]) + v;
       }
     }
-    if memo[0][0] == 0 {
-      println!("Draw");
-    } else if 0 < memo[0][0] {
-      println!("Takahashi");
-    } else {
-      println!("Aoki");
-    }
+  }
+
+  if memo[0][0] == 0 {
+    println!("Draw");
+  } else if memo[0][0] < 0 {
+    println!("Aoki");
+  } else {
+    println!("Takahashi");
+  }
 }
