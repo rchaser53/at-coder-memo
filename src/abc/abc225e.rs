09e606be08204be7a0af6f39a4feaa7b3a9e3d0a
rchaser53 <tayoshizawa29@gmail.com>
2021-10-31 15:18:30 +0900

    add abc225e.rs

---
+++ src/abc/abc225e.rs
@@ -0,0 +1,39 @@
+/** THIS IS AN OUTPUT FILE. NOT EDIT THIS FILE DIRECTLY. **/
+use proconio::input;
+use proconio::marker::*;
+use std::collections::*;
+use std::cmp::Reverse;
+use std::cmp::Ordering;
+
+fn helper(a:&(usize,usize), b:&(usize,usize)) -> Ordering {
+  let v = (a.0 * b.1).cmp(&(a.1 * b.0));
+  if v == Ordering::Less {
+    Ordering::Greater
+  } else if v == Ordering::Greater {
+    Ordering::Less
+  } else {
+    v
+  }
+}
+
+fn main() {
+  input!{
+    n:usize,
+    mut vals:[(usize,usize);n]
+  }
+  let mut points = vec![];
+  for (x, y) in vals {
+    points.push(((x, y-1), (x-1, y)));
+  }
+  points.sort_by(|a,b| helper(&a.1, &b.1));
+
+  let mut result = 0;
+  let mut now = (1, 0);
+  for (from, to) in points {
+    if helper(&now, &from) != Ordering::Greater {
+      result += 1;
+      now = to;
+    }
+  }
+  println!("{}", result);
+}
