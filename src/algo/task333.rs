9ec2eb25b907c1fb24189ab057151e53da109af7
rchaser53 <tayoshizawa29@gmail.com>
2021-12-21 20:08:52 +0900

    add task333.rs

---
+++ src/algo/task333.rs
@@ -0,0 +1,42 @@
+/** THIS IS AN OUTPUT FILE. NOT EDIT THIS FILE DIRECTLY. **/
+use std::collections::*;
+use std::cmp::Reverse;
+
+fn readln<T: std::str::FromStr>() -> T {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  tmp.trim().parse().ok().unwrap()
+}
+fn readvec<T: std::str::FromStr>() -> Vec<T> {
+  readln::<String>()
+      .split_whitespace()
+      .map(|x| x.parse().ok().unwrap())
+      .collect()
+}
+
+fn main() {
+  let n = readln();
+  let mut dict = vec![];
+
+  for _ in 0..n {
+    let v:String = readln();
+    let v = v.chars().into_iter().collect::<Vec<char>>();
+    dict.push(v);
+  }
+
+  let mut memo = vec![vec![0usize;n];n];
+  memo[0][0] = 1;
+
+  for i in 0..n {
+    for j in 0..n {
+      if dict[i][j] == '#' { continue }
+      if 0 < j {
+        memo[i][j] += memo[i][j-1];
+      }
+      if 0 < i {
+        memo[i][j] += memo[i-1][j];
+      }
+    }
+  }
+  println!("{}", memo[n-1][n-1]);
+}
