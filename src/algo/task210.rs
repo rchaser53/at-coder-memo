057eb718539c9063010c3948714de240f4ec252f
rchaser53 <tayoshizawa29@gmail.com>
2021-12-30 09:30:27 +0900

    add task210.rs

---
+++ src/algo/task210.rs
@@ -0,0 +1,34 @@
+/** THIS IS AN OUTPUT FILE. NOT EDIT THIS FILE DIRECTLY. **/
+use std::collections::*;
+use std::cmp::Reverse;
+
+fn readln<T: std::str::FromStr>() -> T {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  tmp.trim().parse().ok().unwrap()
+}
+fn readvec<T: std::str::FromStr>() -> Vec<T> {
+  readln::<String>()
+      .split_whitespace()
+      .map(|x| x.parse().ok().unwrap())
+      .collect()
+}
+fn read_chars() -> Vec<char> {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  let tmp:String = tmp.trim().parse().ok().unwrap();
+  tmp.chars().into_iter().collect::<Vec<char>>()
+}
+
+fn main() {
+  let dict:Vec<usize> = readvec();
+  let (n, m) = (dict[0], dict[1]);
+  let dict:Vec<usize> = readvec();
+  for v in dict {
+    if v == m {
+      println!("Yes");
+      return
+    }
+  }
+  println!("No");
+}
