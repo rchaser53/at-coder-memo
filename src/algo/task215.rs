90ed8d5fd281571852b21d36a00b505f75dd3b2d
rchaser53 <tayoshizawa29@gmail.com>
2021-12-31 10:17:11 +0900

    add task215.rs

---
+++ src/algo/task215.rs
@@ -0,0 +1,33 @@
+use std::collections::*;
+use std::cmp::Reverse;
+
+fn readln<T: std::str::FromStr>() -> T {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  tmp.trim().parse().ok().unwrap()
+}
+fn readvec<T: std::str::FromStr>() -> Vec<T> {
+  readln::<String>()
+      .split_whitespace()
+      .map(|x| x.parse().ok().unwrap())
+      .collect()
+}
+fn read_chars() -> Vec<char> {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  let tmp:String = tmp.trim().parse().ok().unwrap();
+  tmp.chars().into_iter().collect::<Vec<char>>()
+}
+
+fn main() {
+  let dict:Vec<usize> = readvec();
+  let n = dict[0];
+  let dict:Vec<usize> = readvec();
+  let mut result = 0;
+  for i in 1..n {    
+    if dict[i-1] < dict[i] {
+      result += 1;
+    }
+  }
+  println!("{}", result);
+}
