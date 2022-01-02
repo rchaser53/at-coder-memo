f6dbc02b5a164e99f21bac5031878708caa92b83
rchaser53 <tayoshizawa29@gmail.com>
2022-01-02 20:17:17 +0900

    add task377.rs

---
+++ src/algo/task377.rs
@@ -0,0 +1,27 @@
+use std::collections::*;
+use std::cmp::Reverse;
+
+fn readln<T: std::str::FromStr>() -> T {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  tmp.trim().parse().ok().unwrap()
+}
+fn readvec<T: std::str::FromStr>() -> Vec<T> {
+  readln::<String>()
+      .split_whitespace()
+      .map(|x| x.parse().ok().unwrap())
+      .collect()
+}
+fn read_chars() -> Vec<char> {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  let tmp:String = tmp.trim().parse().ok().unwrap();
+  tmp.chars().into_iter().collect::<Vec<char>>()
+}
+
+fn main() {
+  let dict:Vec<isize> = readvec();
+  let (a,b) = (dict[0], dict[1]);
+  println!("{}", std::cmp::max(a+b, a-b));
+  println!("{}", std::cmp::min(a+b, a-b));
+}
