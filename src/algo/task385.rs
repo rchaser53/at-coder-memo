d966fbea6875a0399a25e3eeb642de9f3887210b
rchaser53 <tayoshizawa29@gmail.com>
2022-01-04 12:03:31 +0900

    add task385.rs

---
+++ src/algo/task385.rs
@@ -0,0 +1,33 @@
+use std::collections::*;
+use std::cmp::Reverse;
+
+fn readln<T: std::str::FromStr>() -> T {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  tmp.trim().parse().ok().unwrap()
+}
+fn readvec<T: std::str::FromStr>() -> Vec<T> {
+  readln::<String>()
+      .split_whitespace()
+      .map(|x| x.parse().ok().unwrap())
+      .collect()
+}
+fn read_chars() -> Vec<char> {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  let tmp:String = tmp.trim().parse().ok().unwrap();
+  tmp.chars().into_iter().collect::<Vec<char>>()
+}
+
+fn main() {
+  let _ = read_chars();
+  let s = read_chars();
+  let dict = ['a','i','u','e','o'];
+  let mut result = 0;
+  for c in s {
+    if dict.contains(&c) {
+      result += 1;
+    }
+  }
+  println!("{}", result);
+}
