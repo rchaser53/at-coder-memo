28284202efca7d1c0d5f152b99e47114f00ea3d4
rchaser53 <tayoshizawa29@gmail.com>
2022-01-13 20:48:27 +0900

    add task498.rs

---
+++ src/algo/task498.rs
@@ -0,0 +1,40 @@
+/** THIS IS AN OUTPUT FILE. NOT EDIT THIS FILE DIRECTLY. **/
+use std::collections::*;
+use std::cmp::Reverse;
+
+fn readln<T: std::str::FromStr>() -> T {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  tmp.trim().parse().ok().unwrap()
+}
+fn readvec<T: std::str::FromStr>() -> Vec<T> {
+  readln::<String>()
+      .split_whitespace()
+      .map(|x| x.parse().ok().unwrap())
+      .collect()
+}
+fn read_chars() -> Vec<char> {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  let tmp:String = tmp.trim().parse().ok().unwrap();
+  tmp.chars().into_iter().collect::<Vec<char>>()
+}
+
+fn gcd(a:usize, b:usize) -> usize {
+  if b == 0 {
+    a
+  } else {
+    gcd(b, a % b)
+  }
+}
+
+fn main() {
+  let a:Vec<usize> = readvec();
+  let (a,b,k) = (a[0],a[1],a[2]);
+  let g = gcd(a,b);
+  if k % g == 0 && k <= std::cmp::max(a,b) {
+    println!("Yes");
+  } else {
+    println!("No");
+  }
+}
