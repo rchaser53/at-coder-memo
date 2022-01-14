1de4b52ee54709ff89c353d8d126295ac7263591
rchaser53 <tayoshizawa29@gmail.com>
2022-01-14 20:47:31 +0900

    add task474.rs

---
+++ src/algo/task474.rs
@@ -0,0 +1,41 @@
+/** THIS IS AN OUTPUT FILE. NOT EDIT THIS FILE DIRECTLY. **/
+use std::collections::*;
+use std::cmp::Reverse;
+
+fn readln<T: std::str::FromStr>() -> T {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  tmp.trim().parse().ok().unwrap()
+}
+fn readvec<T: std::str::FromStr>() -> Vec<T> {
+  readln::<String>()
+      .split_whitespace()
+      .map(|x| x.parse().ok().unwrap())
+      .collect()
+}
+fn read_chars() -> Vec<char> {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  let tmp:String = tmp.trim().parse().ok().unwrap();
+  tmp.chars().into_iter().collect::<Vec<char>>()
+}
+
+fn gcd(a: i128, b: i128) -> i128 {
+  if b == 0 {
+    return a
+  }
+  gcd(b, a % b)
+}
+
+fn lcm(a: i128, b: i128) -> i128 {
+  a * b / gcd(a, b)
+}
+
+fn main() {
+  let a:Vec<i128> = readvec();
+  let (a,b,c,d) = (a[0],a[1],a[2],a[3]);
+  let x = (a-c).abs();
+  let y = (b-d).abs();
+
+  println!("{}", gcd(x, y)-1);
+}
