b389ff13ee940219f200d66580ff38aa8baf3c86
rchaser53 <tayoshizawa29@gmail.com>
2022-01-18 18:54:54 +0900

    add task489.rs

---
+++ src/algo/task489.rs
@@ -0,0 +1,33 @@
+/** THIS IS AN OUTPUT FILE. NOT EDIT THIS FILE DIRECTLY. **/
+use std::collections::*;
+use std::cmp::Reverse;
+
+fn readln<T: std::str::FromStr>() -> T {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  tmp.trim().parse().ok().unwrap()
+}
+fn readvec<T: std::str::FromStr>() -> Vec<T> {
+  readln::<String>()
+      .split_whitespace()
+      .map(|x| x.parse().ok().unwrap())
+      .collect()
+}
+fn readchars() -> Vec<char> {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  let tmp:String = tmp.trim().parse().ok().unwrap();
+  tmp.chars().into_iter().collect::<Vec<char>>()
+}
+
+fn main() {
+  let a:Vec<usize> = readvec();
+  let (n,m) = (a[0],a[1]);
+  let mut vals:Vec<isize> = readvec();
+  vals.sort();
+  let arr:Vec<usize> = readvec();
+  
+  for i in 0..m {
+    println!("{}", vals[arr[i]]);
+  }
+}
