5030173bb01c94ecb0f392b4049e2aa31ed32030
rchaser53 <tayoshizawa29@gmail.com>
2022-01-26 21:03:27 +0900

    add task739.rs

---
+++ src/algo/task739.rs
@@ -0,0 +1,34 @@
+/** THIS IS AN OUTPUT FILE. NOT EDIT THIS FILE DIRECTLY. **/
+use std::collections::*;
+use std::cmp::Reverse;
+use std::cmp::*;
+
+fn readln<T: std::str::FromStr>() -> T {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  tmp.trim().parse().ok().unwrap()
+}
+fn readvec<T: std::str::FromStr>() -> Vec<T> {
+  readln::<String>()
+      .split_whitespace()
+      .map(|x| x.parse().ok().unwrap())
+      .collect()
+}
+fn readchars() -> Vec<char> {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  let tmp:String = tmp.trim().parse().ok().unwrap();
+  tmp.chars().into_iter().collect::<Vec<char>>()
+}
+
+fn main() {
+  let a:Vec<usize> = readvec();
+  let n = a[0];
+
+  let mut result = 1usize;
+  for i in 1..=n {
+    result *= i;
+  }
+
+  println!("{}", result);
+}
