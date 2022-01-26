32c2a5e8585e0608094800e721b744c6785052cb
rchaser53 <tayoshizawa29@gmail.com>
2022-01-26 20:56:40 +0900

    add task735.rs

---
+++ src/algo/task735.rs
@@ -0,0 +1,49 @@
+/** THIS IS AN OUTPUT FILE. NOT EDIT THIS FILE DIRECTLY. **/
+use std::collections::*;
+use std::cmp::Reverse;
+use std::cmp::*;
+
+fn readln<T: std::str::FromStr>() -> T {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  tmp.trim().parse().ok().unwrap()
+}
+fn readvec<T: std::str::FromStr>() -> Vec<T> {
+  readln::<String>()
+      .split_whitespace()
+      .map(|x| x.parse().ok().unwrap())
+      .collect()
+}
+fn readchars() -> Vec<char> {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  let tmp:String = tmp.trim().parse().ok().unwrap();
+  tmp.chars().into_iter().collect::<Vec<char>>()
+}
+
+fn main() {
+  let a:Vec<usize> = readvec();
+  let _n = a[0];
+  let b:Vec<usize> = readvec();
+  let mut a = vec![];
+
+  for i in 0..10 {
+    if !b.contains(&i) {
+      a.push(i);
+    }
+  }
+  let n = a.len();
+
+  let mut set = HashSet::new();
+  for i in 0..n {
+    let v1 = a[i] * 100;
+    for j in 0..n {
+      let v2 = a[j] * 10;
+      for k in 0..n {
+        let v3 = a[k];
+        set.insert(v1+v2+v3);
+      }
+    }
+  }
+  println!("{}", 1000-set.len());
+}
