0d92c438dadaf76fe1c476de1ed64c9fb9bedd6a
rchaser53 <tayoshizawa29@gmail.com>
2022-02-03 21:40:02 +0900

    add task763.rs

---
+++ src/algo/task763.rs
@@ -0,0 +1,35 @@
+/** THIS IS AN OUTPUT FILE. NOT EDIT THIS FILE DIRECTLY. **/
+use std::collections::*;
+use std::cmp::Reverse;
+use std::cmp::*;
+
+fn readln<T: std::str::FromStr>() -> T {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  tmp.trim().parse().ok().unwrap()
+}
+fn readvec<T: std::str::FromStr>() -> Vec<T> {
+  readln::<String>()
+      .split_whitespace()
+      .map(|x| x.parse().ok().unwrap())
+      .collect()
+}
+fn readchars() -> Vec<char> {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  let tmp:String = tmp.trim().parse().ok().unwrap();
+  tmp.chars().into_iter().collect::<Vec<char>>()
+}
+
+fn main() {
+  let a:Vec<usize> = readvec();
+  let b:Vec<usize> = readvec();
+  
+  let mut base = 0;
+  for i in 0..a[0] {
+    if (i+1) % a[1] == 0 {
+      base += b[i];
+    }
+  }
+  println!("{}", base as f64 / 100f64);
+}
