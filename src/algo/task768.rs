8c5914a2cde5ea9bbfc7721649334e403e9604d2
rchaser53 <tayoshizawa29@gmail.com>
2022-02-05 13:46:00 +0900

    add task768.rs

---
+++ src/algo/task768.rs
@@ -0,0 +1,35 @@
+/** THIS IS AN OUTPUT FILE. NOT EDIT THIS FILE DIRECTLY. **/
+use std::collections::*;
+use std::cmp::Reverse;
+use std::cmp::*;
+
+fn readln<T: std::str::FromStr>() -> T {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  tmp.trim().parse().ok().unwrap()
+}
+fn readvec<T: std::str::FromStr>() -> Vec<T> {
+  readln::<String>()
+      .split_whitespace()
+      .map(|x| x.parse().ok().unwrap())
+      .collect()
+}
+fn readchars() -> Vec<char> {
+  let mut tmp = String::new();
+  std::io::stdin().read_line(&mut tmp).ok();
+  let tmp:String = tmp.trim().parse().ok().unwrap();
+  tmp.chars().into_iter().collect::<Vec<char>>()
+}
+
+fn main() {
+  let a:Vec<usize> = readvec();
+  let b:Vec<f64> = readvec();
+  let c:Vec<usize> = readvec();
+
+  let mut result = 1f64;
+  for i in c {
+    result *= b[i-1] / 100f64;
+  }
+  
+  println!("{}", result);
+}
