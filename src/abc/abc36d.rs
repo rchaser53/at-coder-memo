e86b40affa816028bad5a7b1cdeec217a931059c
rchaser53 <tayoshizawa29@gmail.com>
2022-02-21 22:07:02 +0900

    rewrite abc36d.rs by myself

--- src/abc/abc36d.rs
+++ src/abc/abc36d.rs
@@ -1,39 +1,40 @@
-/* OUTPUT FILE */
-#![allow(unused_imports)]
+/** THIS IS AN OUTPUT FILE. NOT EDIT THIS FILE DIRECTLY. **/
 use proconio::input;
 use proconio::marker::*;
 use std::collections::*;
+use std::cmp::*;
 
-const MOD: usize = 1_000_000_007;
+const MOD:usize = 1_000_000_007;
 
-fn dfs(g: &Vec<Vec<usize>>, memo: &mut Vec<(usize, usize)>, ci: usize, last: usize) {
-    for &ni in &g[ci] {
-        if ni == last {
-            continue;
-        }
-        dfs(g, memo, ni, ci);
-        memo[ci].0 *= memo[ni].1;
-        memo[ci].1 *= memo[ni].0 + memo[ni].1;
-        memo[ci].0 %= MOD;
-        memo[ci].1 %= MOD;
-    }
+fn dfs(g:&Vec<Vec<usize>>, ci:usize, last:usize, val:(usize,usize)) -> (usize,usize) {
+
+  let mut white = val.0;
+  let mut black = val.1;
+  for &ni in &g[ci] {
+    if ni == last { continue }
+
+    let (w, b) = dfs(g, ni, ci, val);
+    let merge = (w+b) % MOD;
+    white *= merge;
+    white %= MOD;
+    black *= w;
+    black %= MOD;
+  }
+
+  (white, black)
 }
 
-pub fn main(
-) {
-    input! {
-      n:usize,
-      vals:[(Usize1,Usize1);n-1]
-    }
+fn main() {
+  input! {
+    n:usize,
+    edges:[(Usize1,Usize1);n-1]
+  }
 
-    // 0: black 1: white
-    let mut dp = vec![(1, 1); n];
-    let mut g = vec![vec![]; n];
-    for (from, to) in vals {
-        g[from].push(to);
-        g[to].push(from);
-    }
-
-    dfs(&g, &mut dp, 0, 1_000_000_000);
-    println!("{}", (dp[0].0 + dp[0].1) % MOD);
+  let mut g = vec![vec![];n];
+  for (a,b) in edges {
+    g[a].push(b);
+    g[b].push(a);
+  }
+  let v = dfs(&g, 0, 1_000_000_000, (1,1));
+  println!("{}", (v.0 + v.1) % MOD);
 }
