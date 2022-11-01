e6ef35c6973d75de93a5628bdce81ecb99235836
rchaser53 <tayoshizawa29@gmail.com>
2022-11-01 22:41:35 +0900

    update task316.rs

--- src/algo/task316.rs
+++ src/algo/task316.rs
@@ -1,6 +1,7 @@
 /** THIS IS AN OUTPUT FILE. NOT EDIT THIS FILE DIRECTLY. **/
+
+use std::cmp::Ordering;
 use std::collections::*;
-use std::cmp::Reverse;
 
 fn readln<T: std::str::FromStr>() -> T {
   let mut tmp = String::new();
@@ -21,34 +22,23 @@ fn read_chars() -> Vec<char> {
 }
 
 fn main() {
-  let dict:Vec<usize> = readvec();
-  let (n, m) = (dict[0], dict[1]);
-
+  let nm:Vec<usize> = readvec();
+  let n = nm[0];
+  let m = nm[1];
   let mut c:Vec<Vec<usize>> = vec![];
   for _ in 0..n {
     c.push(readvec());
   }
 
-  let inf = 1_000_000_000;
-  let mut memo = vec![vec![inf;m];n];
-  memo[0][0] = c[0][0];
-  for i in 1..m {
-    memo[0][i] = c[0][i] + memo[0][i-1];
-  }
-
+  let inf = 1_000_000_000usize;
+  let mut memo = vec![vec![inf;m+1];n+1];
+  memo[0][0] = 0;
   for i in 0..n {
     for j in 0..m {
-      if 0 < i {
-        memo[i][j] = std::cmp::min(memo[i][j], memo[i-1][j] + c[i][j]);
-      }
-      if 0 < j {
-        memo[i][j] = std::cmp::min(memo[i][j], memo[i][j-1] + c[i][j]);
-      }
-      if 0 < i && 0 < j {
-        memo[i][j] = std::cmp::min(memo[i][j], memo[i-1][j-1] + c[i][j]);
-      }
+      let v = c[i][j];
+      memo[i+1][j+1] = memo[i+1][j+1].min(memo[i][j]+v).min(memo[i][j+1]+v).min(memo[i+1][j]+v);   
     }
   }
 
-  println!("{}", memo[n-1][m-1]);
+  println!("{}", memo[n][m]);
 }
