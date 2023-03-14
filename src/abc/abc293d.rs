376cce92d8de1f9555deb3b266305e8f193c8717
rchaser53 <tayoshizawa29@gmail.com>
2023-03-14 18:53:29 +0900

    add abc293d.rs

---
+++ src/abc/abc293d.rs
@@ -0,0 +1,89 @@
+/** THIS IS AN OUTPUT FILE. NOT EDIT THIS FILE DIRECTLY. **/
+use proconio::input;
+use proconio::marker::*;
+use std::cmp::Ordering;
+use std::cmp::Reverse;
+use std::collections::*;
+
+struct UnionFind {
+  parents: Vec<usize>,
+  sizes: Vec<usize>,
+  ranks: Vec<usize>
+}
+
+impl UnionFind {
+  fn new(n:usize) -> UnionFind {
+    let mut uf = UnionFind { parents: vec![0;n], sizes:vec![1;n], ranks:vec![0;n] };
+    for i in 0..n {
+      uf.parents[i] = i;
+    }
+    uf
+  }
+
+  fn root(&mut self, x:usize) -> usize {
+    let ti = self.parents[x];
+    if ti == x {
+      x
+    } else {
+      self.parents[x] = self.root(ti);
+      self.parents[x]
+    }
+  }
+
+  fn is_same(&mut self, x:usize, y:usize) -> bool {
+    self.root(x) == self.root(y)
+  }
+
+  fn unite(&mut self, x:usize, y:usize) -> bool {
+    let rx = self.root(x);
+    let ry = self.root(y);
+    if rx == ry { return false }
+    let (rx, ry) = if self.ranks[rx] < self.ranks[ry] {
+      (ry, rx)
+    } else {
+      (rx, ry)
+    };
+    self.parents[ry] = rx;
+    if self.ranks[rx] == self.ranks[ry] {
+      self.ranks[rx] += 1;
+    }
+
+    self.sizes[rx] += self.sizes[ry];
+    true
+  }
+
+  fn group_size(&mut self, x:usize) -> usize {
+    let i = self.root(x);
+    self.sizes[i]
+  }
+}
+
+fn main() {
+  input!{
+    n:usize,
+    m:usize,
+    abcd:[(Usize1,char,Usize1,char);m]
+  }
+
+  let mut uf = UnionFind::new(n);
+  let mut looped = HashSet::new();
+  
+  for (a,_,b,_) in abcd {
+    if uf.is_same(a,b) {
+      let pi = uf.root(a);
+      looped.insert(pi);
+    } else {
+      uf.unite(a,b);
+    }
+  }
+
+  let mut unloop = HashSet::new();
+  for i in 0..n {
+    let pi = uf.root(i);
+    if !looped.contains(&pi) {
+      unloop.insert(pi);
+    }
+  }
+
+  println!("{} {}", looped.len(), unloop.len());
+}
