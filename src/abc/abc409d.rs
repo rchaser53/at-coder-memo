5fa970b15b7eafd563250ac1849d1accf4d5f7a3
rchaser53 <tayoshizawa29@gmail.com>
2025-06-16 08:45:16 +0900

    fix abc409d.rs answer

--- src/abc/abc409d.rs
+++ src/abc/abc409d.rs
@@ -10,53 +10,29 @@ fn main() {
     t:usize,
   }
 
+  // cba => bac
+  // cbd => bdc x => bcd
   for _ in 0..t {
     input! {
       n:usize,
       mut s:Chars,
     }
-
-    let mut memo = vec![-1;26];
-
-    let mut l = n-1;
-    let mut r = n-1;
-    for i in (0..n).rev() {
-      let c = s[i] as usize - 'a' as usize;
-      let ii = i as i64;
-      memo[c] = memo[c].max(ii);
-
-      // 自分より大きい最小のindexを求める
-      let mut min = n;
-      for j in c+1..26 {
-        if memo[j] != -1 {
-          min = min.min(memo[j] as usize);
+    let mut ci = 0;
+    let mut shoud_change = false;
+    for i in 1..n {
+      if shoud_change {
+        if s[ci] < s[i] {
+          break
         }
-      }
-
-      // 自分より小さい最大のindexを求める
-      let mut max = -1;
-      for j in (0..c).rev() {
-        if memo[j] != -1 {
-          max = memo[j];
+        s.swap(ci, i);
+        ci = i;
+      } else {
+        if s[ci] > s[i] {
+          shoud_change = true;
+          s.swap(ci, i);
         }
+        ci = i;
       }
-
-      if max == -1 {
-        continue
-      }
-      l = i;
-
-      if min < max as usize && 1 < min {
-        r = min;
-      } else if (max as usize) < min {
-        r = max as usize + 1;
-      }
-    }
-
-    if l != n - 1 {
-      // println!("l: {}, r: {} s:{}", l, r,  s.iter().collect::<String>());
-      s.insert(r, s[l]);
-      s.remove(l);
     }
 
     println!("{}", s.iter().collect::<String>());
